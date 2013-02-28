353a7e6d14ce73dbd7a0bad7457f152f4f4d23cf
Albertas Agejevas <alga@pov.lt>
2013-02-28 19:11:14 +0200

    Back to development: 1.0.0a3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='z3c.testing',
-      version='1.0.0a2',
+      version='1.0.0a3.dev0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='High-level Testing Support',
