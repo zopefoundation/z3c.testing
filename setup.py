10cba83d3ecf557a0fdbf7caab9940de2e882ba3
Michael Howitz <icemac@gmx.net>
2025-02-14 10:59:19 +0100

    Back to development: 2.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 
 setup(name='z3c.testing',
-      version='2.0',
+      version='2.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='High-level Testing Support',
